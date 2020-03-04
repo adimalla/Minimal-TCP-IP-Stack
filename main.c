4a3b0a6100d46114d4062958716bd96fb22aeda3
Aditya Mall <aditya.mall1990@gmail.com>
2020-03-04 15:02:45 -0600

    new dir created

--- main.c
+++ main.c
@@ -1,25 +1,54 @@
-// Ethernet Example
-// Jason Losh
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Aditya Mall,
+ * @brief   TCP protocol header file
+ *
+ *  Info
+ *       Target Platform        : EK-TM4C123GXL w/ ENC28J60
+ *       Target uC              : TM4C123GH6PM
+ *       System Clock           : 40 MHz
+ *
+ *       Hardware configuration :-
+ *                               ENC28J60 Ethernet controller
+ *                               MOSI (SSI2Tx) on PB7controller
+ *                               MISO (SSI2Rx) on PB6
+ *                               SCLK (SSI2Clk) on PB4
+ *                               ~CS connected to PB1
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT(c) 2019 Aditya Mall, MIT License </center></h2>
+ *
+ * Copyright (c) 2019 Aditya Mall
+ *
+ * Please Take prior permission from Dr. Jason Losh and Respective Owners of the,
+ * API Libraries if you are a student at The University of Texas at Arlington and,
+ * wish to use part of the code in your project.
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
 
-//-----------------------------------------------------------------------------
-// Hardware Target
-//-----------------------------------------------------------------------------
 
-// Target Platform: EK-TM4C123GXL w/ ENC28J60
-// Target uC:       TM4C123GH6PM
-// System Clock:    40 MHz
 
-// Hardware configuration:
-// ENC28J60 Ethernet controller
-//   MOSI (SSI2Tx) on PB7controller
-//   MISO (SSI2Rx) on PB6
-//   SCLK (SSI2Clk) on PB4
-//   ~CS connected to PB1
-
-//-----------------------------------------------------------------------------
-// Device includes, defines, and assembler directives
-//-----------------------------------------------------------------------------
-
+/*
+ * Standard header and API header files
+ */
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -41,6 +70,21 @@
 #include "mqtt_client.h"
 
 
+
+/******************************************************************************/
+/*                                                                            */
+/*                      Data Structures and Defines                           */
+/*                                                                            */
+/******************************************************************************/
+
+
+#define STATIC    0
+#define ICMP_TEST 1
+#define UDP_TEST  0
+#define TCP_TEST  0
+#define MQTT_TEST 1
+
+
 #define RED_LED      (*((volatile uint32_t *)(0x42000000 + (0x400253FC-0x40000000)*32 + 1*4)))
 #define GREEN_LED    (*((volatile uint32_t *)(0x42000000 + (0x400253FC-0x40000000)*32 + 3*4)))
 #define BLUE_LED     (*((volatile uint32_t *)(0x42000000 + (0x400253FC-0x40000000)*32 + 2*4)))
@@ -48,11 +92,26 @@
 
 
 
-//-----------------------------------------------------------------------------
-// Subroutines
-//-----------------------------------------------------------------------------
+typedef enum _app_state
+{
+    APP_INIT   = 0,
+    APP_READ   = 1,
+    APP_WRITE  = 2,
+    USER_INPUT = 3,
 
-// Initialize Hardware
+}app_state_t;
+
+
+
+
+
+/******************************************************************************/
+/*                                                                            */
+/*                       Functions Implementations                            */
+/*                                                                            */
+/******************************************************************************/
+
+
 void initHw()
 {
     // Configure HW to work with 16 MHz XTAL, PLL enabled, system clock of 40 MHz
@@ -71,11 +130,7 @@ void initHw()
     GPIO_PORTF_DEN_R = 0x1E;  // enable LEDs and pushbuttons
     GPIO_PORTF_PUR_R = 0x1E;  // enable internal pull-up for push button
 
-    /* ether module interrupt */
-
-
-
-#if IOT_COURSE_TEST
+#if IOT_COURSE_TEST /* IOT Networking Student Hardware check */
 
     // Configure ~CS for ENC28J60
     GPIO_PORTA_DIR_R = (1 << 3);  // make bit 1 an output
@@ -101,25 +156,26 @@ void initHw()
 #else
 
     // Configure ~CS for ENC28J60
-    GPIO_PORTB_DIR_R = (1 << 5);  // make bit 1 an output
-    //GPIO_PORTB_DR2R_R = 0x02; // set drive strength to 2mA
-    GPIO_PORTB_DEN_R = (1 << 5);  // enable bits 1 for digital
+    GPIO_PORTB_DIR_R = (1 << 5);                    // make bit 1 an output
+    GPIO_PORTB_DEN_R = (1 << 5);                    // enable bits 1 for digital
 
     // Configure SSI2 pins for SPI configuration
-    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R2;           // turn-on SSI2 clocking
-    GPIO_PORTB_DIR_R |= 0x90;                        // make bits 4 and 7 outputs
-    GPIO_PORTB_DR2R_R |= 0x90;                       // set drive strength to 2mA
-    GPIO_PORTB_AFSEL_R |= 0xD0;                      // select alternative functions for MOSI, MISO, SCLK pins
-    GPIO_PORTB_PCTL_R = GPIO_PCTL_PB7_SSI2TX | GPIO_PCTL_PB6_SSI2RX | GPIO_PCTL_PB4_SSI2CLK; // map alt fns to SSI2
-    GPIO_PORTB_DEN_R |= 0xD0;                        // enable digital operation on TX, RX, CLK pins
+    SYSCTL_RCGCSSI_R   |= SYSCTL_RCGCSSI_R2;        // turn-on SSI2 clocking
+    GPIO_PORTB_DIR_R   |= 0x90;                     // make bits 4 and 7 outputs
+    GPIO_PORTB_DR2R_R  |= 0x90;                     // set drive strength to 2mA
+    GPIO_PORTB_AFSEL_R |= 0xD0;                     // select alternative functions for MOSI, MISO, SCLK pins
+    GPIO_PORTB_PCTL_R   = GPIO_PCTL_PB7_SSI2TX | \
+                          GPIO_PCTL_PB6_SSI2RX | \
+                          GPIO_PCTL_PB4_SSI2CLK;    // map alt fns to SSI2
+    GPIO_PORTB_DEN_R   |= 0xD0;                     // enable digital operation on TX, RX, CLK pins
 
     // Configure the SSI2 as a SPI master, mode 3, 8bit operation, 1 MHz bit rate
-    SSI2_CR1_R &= ~SSI_CR1_SSE;                      // turn off SSI2 to allow re-configuration
-    SSI2_CR1_R = 0;                                  // select master mode
-    SSI2_CC_R = 0;                                   // select system clock as the clock source
-    SSI2_CPSR_R = 40;                                // set bit rate to 1 MHz (if SR=0 in CR0)
-    SSI2_CR0_R = SSI_CR0_FRF_MOTO | SSI_CR0_DSS_8;   // set SR=0, mode 0 (SPH=0, SPO=0), 8-bit
-    SSI2_CR1_R |= SSI_CR1_SSE;                       // turn on SSI2
+    SSI2_CR1_R &= ~SSI_CR1_SSE;                     // turn off SSI2 to allow re-configuration
+    SSI2_CR1_R  = 0;                                // select master mode
+    SSI2_CC_R   = 0;                                // select system clock as the clock source
+    SSI2_CPSR_R = 40;                               // set bit rate to 1 MHz (if SR=0 in CR0)
+    SSI2_CR0_R  = SSI_CR0_FRF_MOTO | SSI_CR0_DSS_8; // set SR=0, mode 0 (SPH=0, SPO=0), 8-bit
+    SSI2_CR1_R |= SSI_CR1_SSE;                      // turn on SSI2
 #endif
 
 }
@@ -127,7 +183,6 @@ void initHw()
 
 void init_uart0(void)
 {
-
     // Enable GPIO port F peripherals
     SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOA;
 
@@ -146,7 +201,6 @@ void init_uart0(void)
     UART0_CTL_R  |= UART_CTL_TXE | UART_CTL_RXE | UART_CTL_UARTEN;  // enable TX, RX, and module
 
 }
-
 
 
 // Blocking function that writes a serial character when the UART buffer is not full
@@ -178,37 +232,36 @@ void putsUart0(const char* str)
 
 
 
-
 void init_adc(void)
 {
 
     // Enable GPIO port B and E peripherals
 
     // Configure AN0 as an analog input
-    SYSCTL_RCGCADC_R |= SYSCTL_RCGCADC_R0;                           // turn on ADC module 0 clocking
-    GPIO_PORTE_AFSEL_R |= (1 << 3);                      // select alternative functions for AN0 (PE3)
-    GPIO_PORTE_DEN_R &= ~(1 << 3);                       // turn off digital operation on pin PE3
-    GPIO_PORTE_AMSEL_R |= (1 << 3);                      // turn on analog operation on pin PE3
-    ADC0_CC_R = ADC_CC_CS_SYSPLL;                    // select PLL as the time base (not needed, since default value)
-    ADC0_ACTSS_R &= ~ADC_ACTSS_ASEN3;                // disable sample sequencer 3 (SS3) for programming
-    ADC0_EMUX_R = ADC_EMUX_EM3_PROCESSOR;            // select SS3 bit in ADCPSSI as trigger
-    ADC0_SSMUX3_R = 0;                               // set first sample to AN0
-    ADC0_SSCTL3_R = ADC_SSCTL3_END0 | ADC_SSCTL3_TS0;                 // mark first sample as the end
-    ADC0_ACTSS_R |= ADC_ACTSS_ASEN3;                 // enable SS3 for operation
+    SYSCTL_RCGCADC_R |= SYSCTL_RCGCADC_R0;             // turn on ADC module 0 clocking
+    GPIO_PORTE_AFSEL_R |= (1 << 3);                    // select alternative functions for AN0 (PE3)
+    GPIO_PORTE_DEN_R &= ~(1 << 3);                     // turn off digital operation on pin PE3
+    GPIO_PORTE_AMSEL_R |= (1 << 3);                    // turn on analog operation on pin PE3
+    ADC0_CC_R = ADC_CC_CS_SYSPLL;                      // select PLL as the time base (not needed, since default value)
+    ADC0_ACTSS_R &= ~ADC_ACTSS_ASEN3;                  // disable sample sequencer 3 (SS3) for programming
+    ADC0_EMUX_R = ADC_EMUX_EM3_PROCESSOR;              // select SS3 bit in ADCPSSI as trigger
+    ADC0_SSMUX3_R = 0;                                 // set first sample to AN0
+    ADC0_SSCTL3_R = ADC_SSCTL3_END0 | ADC_SSCTL3_TS0;  // mark first sample as the end
+    ADC0_ACTSS_R |= ADC_ACTSS_ASEN3;                   // enable SS3 for operation
 
 }
 
 
 uint16_t readAdc0Ss3()
 {
-    ADC0_PSSI_R |= ADC_PSSI_SS3;                     // set start bit
-    while (ADC0_ACTSS_R & ADC_ACTSS_BUSY);           // wait until SS3 is not busy
-    return ADC0_SSFIFO3_R & 0x0F;                           // get single result from the FIFO
+    ADC0_PSSI_R |= ADC_PSSI_SS3;            // set start bit
+    while (ADC0_ACTSS_R & ADC_ACTSS_BUSY);  // wait until SS3 is not busy
+    return ADC0_SSFIFO3_R & 0x0F;           // get single result from the FIFO
 }
 
 
 
-/* wrapper function */
+/* wrapper Functions */
 
 uint8_t ether_open(uint8_t *mac_address)
 {
@@ -236,6 +289,8 @@ uint8_t myPutChar(char data)
 }
 
 
+
+/* Link Console operation functions */
 console_ops_t myUartOperations =
 {
 
@@ -246,25 +301,19 @@ console_ops_t myUartOperations =
 };
 
 
-
-typedef enum _app_state
+/* Link Network operation functions */
+ether_operations_t ether_ops =
 {
-    APP_INIT   = 0,
-    APP_READ   = 1,
-    APP_WRITE  = 2,
-    USER_INPUT = 3,
-
-}app_state_t;
-
-
+ .open                     = ether_open,
+ .network_interface_status = etherKbhit,
+ .ether_send_packet        = etherPutPacket,
+ .ether_recv_packet        = etherGetPacket,
+ .random_gen_seed          = readAdc0Ss3,
+};
 
 
-#define STATIC    0
-#define ICMP_TEST 1
-#define UDP_TEST  0
-#define TCP_TEST  0
-#define MQTT_TEST 1
 
+/* Main */
 int main(void)
 {
 
@@ -294,16 +343,6 @@ int main(void)
     my_console = console_open(&myUartOperations, 115200, serial_buffer, CONSOLE_STATIC);
 
     console_print(my_console, CONSOLE_CLEAR_SCREEN);
-
-    /* Link network operation functions */
-    ether_operations_t ether_ops =
-    {
-     .open                     = ether_open,
-     .network_interface_status = etherKbhit,
-     .ether_send_packet        = etherPutPacket,
-     .ether_recv_packet        = etherGetPacket,
-     .random_gen_seed          = readAdc0Ss3,
-    };
 
     /* Create Ethernet handle */
     ethernet = create_ethernet_handle(&network_hardware->data, "02:03:04:50:60:48", "192.168.1.199", &ether_ops);
